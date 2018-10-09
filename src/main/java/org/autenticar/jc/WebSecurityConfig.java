549ceb7be2782c7c58619e9205875057f813812e
Juan Carlos Campos <rapidclimate@gmail.com>
2018-10-09 11:37:11 -0500

    auntenticacion basica

--- src/main/java/org/autenticar/jc/WebSecurityConfig.java
+++ src/main/java/org/autenticar/jc/WebSecurityConfig.java
@@ -65,11 +65,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("HEAD",
                 "GET", "POST", "PUT", "DELETE", "PATCH"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+
+
         configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
+        // setAllowedHeaders este es importantisimo sion el se lanza el eeror 403 de origenes no permitidos
+        //
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
